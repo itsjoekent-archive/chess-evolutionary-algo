689c260f6d0cd9cd3c41406c22892180f3b9fb93
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2024-02-22 17:07:15 -0500

    more runner work

--- packages/runner/src/index.ts
+++ packages/runner/src/index.ts
@@ -1,13 +1,21 @@
 import { cpus } from 'node:os';
-import { Worker, workerData, isMainThread } from 'node:worker_threads';
+import {
+  Worker,
+  workerData,
+  isMainThread,
+  parentPort,
+} from 'node:worker_threads';
+import { fileURLToPath } from 'node:url';
 import { Storage } from '@google-cloud/storage';
 import {
   System,
   InstructionSet,
 } from '@chess-evolutionary-algo/pattern-engine';
 
-const bucket = process.env.STORAGE_BUCKET as string;
-if (!bucket) {
+const __filename = fileURLToPath(import.meta.url);
+
+const bucketName = process.env.STORAGE_BUCKET as string;
+if (!bucketName) {
   throw new Error('STORAGE_BUCKET not defined');
 }
 
@@ -15,6 +23,12 @@ const machineName = process.env.MACHINE_NAME as string;
 if (!machineName) {
   throw new Error('MACHINE_NAME not defined');
 }
+
+const loggerName = `runner-${machineName}-${workerData?.threadId || 'alpha'}`;
+const log = (msg: string) =>
+  isMainThread
+    ? console.log(`${loggerName} - ${msg}`)
+    : parentPort!.postMessage(`${loggerName} - ${msg}`);
 
 const storage = new Storage();
 
@@ -27,12 +41,18 @@ const threadIds = [
   'foxtrot',
   'golf',
   'hotel',
+  'india',
+  'juliett',
+  'kilo',
+  'lima',
+  'mike',
+  'november',
 ];
 
 async function loadTopPlayers(fromMachineId: string, fromThreadId: string) {
   const data = await storage
-    .bucket(bucket)
-    .file(`tournaments/${fromMachineId}/${fromThreadId}/latest.json`)
+    .bucket(bucketName)
+    .file(`tournaments/${fromMachineId}/${fromThreadId}/latest`)
     .download();
   const { players } = JSON.parse(data.toString());
   return players as InstructionSet[];
@@ -40,7 +60,7 @@ async function loadTopPlayers(fromMachineId: string, fromThreadId: string) {
 
 async function getRandomTopPlayers() {
   const [files] = await storage
-    .bucket(bucket)
+    .bucket(bucketName)
     .getFiles({ prefix: 'tournament/' });
   const randomFile = files[Math.floor(Math.random() * files.length)];
   const data = await randomFile.download();
@@ -48,38 +68,43 @@ async function getRandomTopPlayers() {
   return players as InstructionSet[];
 }
 
+async function uploadFile(key: string, data: any) {
+  try {
+    await storage.bucket(bucketName).file(key).save(JSON.stringify(data), {
+      contentType: 'application/json',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function main(threadId: string) {
+  let uploads: Parameters<typeof uploadFile>[] = [];
+
   let tournamentCount = 0;
   let existingPlayers: Record<string, InstructionSet> = {};
+
+  log(`loading prior top players ...`);
 
   try {
     const topPlayers = await loadTopPlayers(machineName, threadId);
     existingPlayers[topPlayers[0].id] = topPlayers[0];
     existingPlayers[topPlayers[1].id] = topPlayers[1];
+    log(`loaded top players => ${topPlayers.map((p) => p.id).join(', ')}`);
   } catch (error) {}
 
   const system = new System({ players: existingPlayers });
 
-  system.subscribe('spawned', (event) => {
-    storage
-      .bucket(bucket)
-      .file(`players/${event.payload.instructionSet.id}.json`)
-      .save(JSON.stringify(event.payload));
-  });
-
   system.subscribe('started_game', () => {
-    console.log(`[game started] ...`);
+    log(`game started ...`);
   });
 
   system.subscribe('game_ended', (event) => {
-    console.log(
-      `[game ended] fen: "${event.payload.fen}", winning score: ${event.payload.winningFitnessScore}, losing score: ${event.payload.losingFitnessScore}, winning player: "${event.payload.winner}", losing player: "${event.payload.loser}"`,
+    log(
+      `game ended => fen: "${event.payload.fen}", winning score: ${event.payload.winningFitnessScore}, losing score: ${event.payload.losingFitnessScore}, winning player: "${event.payload.winner}", losing player: "${event.payload.loser}"`,
     );
 
-    storage
-      .bucket(bucket)
-      .file(`games/${event.payload.id}.json`)
-      .save(JSON.stringify(event.payload));
+    uploads.push([`games/${event.payload.id}`, event.payload]);
   });
 
   system.subscribe('tournament_ended', (event) => {
@@ -96,29 +121,41 @@ async function main(threadId: string) {
       Object.assign({}, players[playerFitnessScores[1]]),
     ];
 
-    storage
-      .bucket(bucket)
-      .file(`tournaments/${machineName}/${threadId}/latest.json`)
-      .save(JSON.stringify({ players: topPlayers }));
+    uploads.push([
+      `tournaments/${machineName}/${threadId}/latest`,
+      { players: topPlayers },
+    ]);
+
+    Object.keys(players).forEach((id) => {
+      uploads.push([`players/${id}`, players[id]]);
+    });
   });
 
   async function run() {
+    log(`running tournament ...`);
     const result = system.startTournament();
     system.evolvePlayers(result);
 
     if (tournamentCount > 0 && tournamentCount % 10 === 0) {
       const topPlayers = await getRandomTopPlayers();
-      console.log(
-        `[migrating top players] ${topPlayers.map((p) => p.id).join(', ')}`,
-      );
+      log(`migrating top players => ${topPlayers.map((p) => p.id).join(', ')}`);
       system.migration(topPlayers);
     }
 
+    if (uploads.length) {
+      for (const [key, data] of uploads) {
+        log(`uploading file => ${key}`);
+        await uploadFile(key, data);
+      }
+      uploads = [];
+    }
+
     tournamentCount++;
-    run();
+    setTimeout(run, 0);
   }
 
-  system.setupTournament();
+  log(`setting up tournament ...`);
+  system.setupTournament(4);
   run();
 }
 
@@ -128,22 +165,37 @@ function startWorker(threadId: string) {
   });
 
   worker.on('error', (error) => {
-    console.log(`[worker exit] (${threadId}) error: ${error}`);
+    log(`worker exit => error: ${error}`);
   });
 
   worker.on('exit', (code) => {
     if (code !== 0) {
-      console.log(`[worker exit] (${threadId}) code: ${code}`);
+      log(`worker exit => code: ${code}`);
     }
 
     startWorker(threadId);
   });
+
+  worker.on('message', (message) => {
+    console.log(message);
+  });
 }
 
+process.on('unhandledRejection', (error: any) => {
+  console.error(error?.message);
+  process.exitCode = 1;
+});
+
+await storage.getServiceAccount();
+
 if (isMainThread) {
-  for (let i = 0; i < cpus().length; i++) {
-    startWorker(threadIds[i]);
+  const workers = cpus().length - 1;
+  log(`spawning ${workers} workers ...`);
+  for (let i = 0; i < workers; i++) {
+    startWorker(threadIds[i + 1]);
   }
+
+  setTimeout(() => main(threadIds[0]), 1000);
 } else {
   main(workerData.threadId);
 }
